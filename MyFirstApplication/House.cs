11d8fce7c448319bf3adb0a24a3d60e1300733dc
PCarawayXC <pcarawayxc@gmail.com>
2023-09-29 13:28:50 -0500

    CAC-103 C# Overview

---
+++ MyFirstApplication/House.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFirstApplication;
+public class House
+{
+    string foundation;
+    string roofType;
+    string doorColor;
+    int windowSize;
+
+    public void DoorOpenClose()
+    {
+        Console.WriteLine("My door is open.");
+    }
+}
